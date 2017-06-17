7c179935e6656b2ccb32f491739a6dc2d94e128e
saikumar0218 <sai.infosec@gmail.com>
2017-06-17 15:50:50 +0530

    Update BaseController.java

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -19,7 +19,7 @@ public class BaseController {
 
 		model.addAttribute("message", "Sai Kumar");
 		model.addAttribute("counter", ++counter);
-		logger.debug("[welcome] counter : {}", counter);
+		logger.debug("[Sai kumar] counter : {}", counter);
 
 		// Spring uses InternalResourceViewResolver and return back index.jsp
 		return VIEW_INDEX;
